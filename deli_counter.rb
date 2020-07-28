3c6d9afea022ba93800e12757c6b74d09b718a90
Josh Raiborde <joshsrai@yahoo.com>
2020-07-28 23:49:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,3 +8,7 @@ def line(line)
   puts line.size == 0 ? "#{empty} empty." : "#{empty}:" + line.map.with_index{ |name, index| " #{index + 1}. #{name}"}.join
 end
 
+def take_a_number(line, name)
+  line << name
+  puts "Welcome #{name}. You are number #{line.size} in line"
+end
